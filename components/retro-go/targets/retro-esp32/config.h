9caba77bb25e1a0f5a1a6764db5a3b0cf9e2a0fe
Pilothorizon <pilothorizon2011@gmail.com>
2026-03-19 14:52:01 +0530

    Update config.h
    for my wiring and st7789

--- components/retro-go/targets/retro-esp32/config.h
+++ components/retro-go/targets/retro-esp32/config.h
@@ -5,86 +5,71 @@
 #define RG_STORAGE_ROOT             "/sd"
 #define RG_STORAGE_SDSPI_HOST       SPI2_HOST
 #define RG_STORAGE_SDSPI_SPEED      SDMMC_FREQ_DEFAULT
-// #define RG_STORAGE_SDMMC_HOST       SDMMC_HOST_SLOT_1
-// #define RG_STORAGE_SDMMC_SPEED      SDMMC_FREQ_DEFAULT
-// #define RG_STORAGE_FLASH_PARTITION  "vfs"
 
-// Audio
-#define RG_AUDIO_USE_INT_DAC        3   // 0 = Disable, 1 = GPIO25, 2 = GPIO26, 3 = Both
-#define RG_AUDIO_USE_EXT_DAC        1   // 0 = Disable, 1 = Enable
+// Audio - using internal DAC (no external DAC assumed)
+#define RG_AUDIO_USE_INT_DAC        1   // GPIO25
+#define RG_AUDIO_USE_EXT_DAC        0
 
 // Video
 #define RG_SCREEN_DRIVER            0   // 0 = ILI9341/ST7789
 #define RG_SCREEN_HOST              SPI2_HOST
 #define RG_SCREEN_SPEED             SPI_MASTER_FREQ_40M
 #define RG_SCREEN_BACKLIGHT         1
-#define RG_SCREEN_WIDTH             320
-#define RG_SCREEN_HEIGHT            240
+#define RG_SCREEN_WIDTH             240
+#define RG_SCREEN_HEIGHT            320
 #define RG_SCREEN_ROTATE            0
-#define RG_SCREEN_VISIBLE_AREA      {15, 0, 20, 0}
+#define RG_SCREEN_VISIBLE_AREA      {0, 0, 0, 0}
 #define RG_SCREEN_SAFE_AREA         {0, 0, 0, 0}
-#define RG_SCREEN_INIT()                                                                                         \
-    ILI9341_CMD(0xCF, 0x00, 0xc3, 0x30);                                                                         \
-    ILI9341_CMD(0xED, 0x64, 0x03, 0x12, 0x81);                                                                   \
-    ILI9341_CMD(0xE8, 0x85, 0x00, 0x78);                                                                         \
-    ILI9341_CMD(0xCB, 0x39, 0x2c, 0x00, 0x34, 0x02);                                                             \
-    ILI9341_CMD(0xF7, 0x20);                                                                                     \
-    ILI9341_CMD(0xEA, 0x00, 0x00);                                                                               \
-    ILI9341_CMD(0xC0, 0x1B);                 /* Power control   //VRH[5:0] */                                    \
-    ILI9341_CMD(0xC1, 0x12);                 /* Power control   //SAP[2:0];BT[3:0] */                            \
-    ILI9341_CMD(0xC5, 0x32, 0x3C);           /* VCM control */                                                   \
-    ILI9341_CMD(0xC7, 0x91);                 /* VCM control2 */                                                  \
-    ILI9341_CMD(0x36, 0xC8);                 /* Memory Access Control (MY|MX|BGR) */                             \
-    ILI9341_CMD(0xB1, 0x00, 0x10);           /* Frame Rate Control (1B=70, 1F=61, 10=119) */                     \
-    ILI9341_CMD(0xB6, 0x0A, 0xA2);           /* Display Function Control */                                      \
-    ILI9341_CMD(0xF6, 0x01, 0x30);                                                                               \
-    ILI9341_CMD(0xF2, 0x00);                 /* 3Gamma Function Disable */                                       \
-    ILI9341_CMD(0x26, 0x01);                 /* Gamma curve selected */                                          \
-    ILI9341_CMD(0xE0, 0x0F, 0x31, 0x2B, 0x0C, 0x0E, 0x08, 0x4E, 0xF1, 0x37, 0x07, 0x10, 0x03, 0x0E, 0x09, 0x00); \
-    ILI9341_CMD(0xE1, 0x00, 0x0E, 0x14, 0x03, 0x11, 0x07, 0x31, 0xC1, 0x48, 0x08, 0x0F, 0x0C, 0x31, 0x36, 0x0F);
+#define RG_SCREEN_INIT()                                                        \
+    ILI9341_CMD(0x01);                  /* Software Reset */                    \
+    RG_SLEEP_MS(120);                                                           \
+    ILI9341_CMD(0x11);                  /* Sleep Out */                         \
+    RG_SLEEP_MS(120);                                                           \
+    ILI9341_CMD(0x36, 0x00);            /* Memory Access Control */             \
+    ILI9341_CMD(0x3A, 0x55);            /* Pixel Format: 16bit RGB565 */        \
+    ILI9341_CMD(0xB2, 0x0C, 0x0C, 0x00, 0x33, 0x33); /* Porch Setting */       \
+    ILI9341_CMD(0xB7, 0x35);            /* Gate Control */                      \
+    ILI9341_CMD(0xBB, 0x19);            /* VCOM Setting */                      \
+    ILI9341_CMD(0xC0, 0x2C);            /* LCM Control */                       \
+    ILI9341_CMD(0xC2, 0x01);            /* VDV and VRH Command Enable */        \
+    ILI9341_CMD(0xC3, 0x12);            /* VRH Set */                           \
+    ILI9341_CMD(0xC4, 0x20);            /* VDV Set */                           \
+    ILI9341_CMD(0xC6, 0x0F);            /* Frame Rate Control: 60Hz */          \
+    ILI9341_CMD(0xD0, 0xA4, 0xA1);      /* Power Control 1 */                   \
+    ILI9341_CMD(0xE0, 0xD0, 0x04, 0x0D, 0x11, 0x13, 0x2B, 0x3F, 0x54, 0x4C, 0x18, 0x0D, 0x0B, 0x1F, 0x23); /* Positive Gamma */  \
+    ILI9341_CMD(0xE1, 0xD0, 0x04, 0x0C, 0x11, 0x13, 0x2C, 0x3F, 0x44, 0x51, 0x2F, 0x1F, 0x1F, 0x20, 0x23); /* Negative Gamma */  \
+    ILI9341_CMD(0x21);                  /* Display Inversion On */              \
+    ILI9341_CMD(0x29);                  /* Display On */                        \
+    RG_SLEEP_MS(10);
 
-// Input
-// Refer to rg_input.h to see all available RG_KEY_* and RG_GAMEPAD_*_MAP types
-#define RG_GAMEPAD_ADC_MAP {\
-    {RG_KEY_UP,    ADC_UNIT_1, ADC_CHANNEL_7, ADC_ATTEN_DB_11, 2048, 4096},\
-    {RG_KEY_DOWN,  ADC_UNIT_1, ADC_CHANNEL_7, ADC_ATTEN_DB_11, 1024, 2047},\
-    {RG_KEY_LEFT,  ADC_UNIT_1, ADC_CHANNEL_6, ADC_ATTEN_DB_11, 2048, 4096},\
-    {RG_KEY_RIGHT, ADC_UNIT_1, ADC_CHANNEL_6, ADC_ATTEN_DB_11, 1024, 2047},\
-}
+// Input - Simple GPIO buttons, all with pullup, active LOW
+// Connect one leg to GPIO, other leg to GND
 #define RG_GAMEPAD_GPIO_MAP {\
-    {RG_KEY_SELECT, .num = GPIO_NUM_27, .pullup = 1, .level = 0},\
-    {RG_KEY_START,  .num = GPIO_NUM_39, .pullup = 0, .level = 0},\
+    {RG_KEY_UP,     .num = GPIO_NUM_35, .pullup = 0, .level = 0},\
+    {RG_KEY_DOWN,   .num = GPIO_NUM_27, .pullup = 1, .level = 0},\
+    {RG_KEY_LEFT,   .num = GPIO_NUM_36, .pullup = 0, .level = 0},\
+    {RG_KEY_RIGHT,  .num = GPIO_NUM_39, .pullup = 0, .level = 0},\
+    {RG_KEY_SELECT, .num = GPIO_NUM_25, .pullup = 1, .level = 0},\
+    {RG_KEY_START,  .num = GPIO_NUM_26, .pullup = 1, .level = 0},\
     {RG_KEY_A,      .num = GPIO_NUM_32, .pullup = 1, .level = 0},\
     {RG_KEY_B,      .num = GPIO_NUM_33, .pullup = 1, .level = 0},\
-}
-#define RG_GAMEPAD_VIRT_MAP {\
-    {RG_KEY_MENU,   .src = RG_KEY_START | RG_KEY_SELECT},\
-    {RG_KEY_OPTION, .src = RG_KEY_SELECT | RG_KEY_A    },\
+    {RG_KEY_MENU,   .num = GPIO_NUM_34, .pullup = 0, .level = 0},\
 }
 
-// Battery
-#define RG_BATTERY_DRIVER           1
-#define RG_BATTERY_ADC_UNIT         ADC_UNIT_1
-#define RG_BATTERY_ADC_CHANNEL      ADC_CHANNEL_0
-#define RG_BATTERY_CALC_PERCENT(raw) (((raw) * 2.f - 3500.f) / (4200.f - 3500.f) * 100.f)
-#define RG_BATTERY_CALC_VOLTAGE(raw) ((raw) * 2.f * 0.001f)
+// Note: GPIO 34, 35, 36, 39 are INPUT ONLY on ESP32 - no internal pullup!
+// For those pins, add a 10k resistor from the pin to 3.3V externally
 
-
-// Status LED
-#define RG_GPIO_LED                 GPIO_NUM_2
-
-// I2C BUS
-// #define RG_GPIO_I2C_SDA             GPIO_NUM_15
-// #define RG_GPIO_I2C_SCL             GPIO_NUM_4
+// Battery - disabled, no battery monitoring
+#define RG_BATTERY_DRIVER           0
 
 // SPI Display
 #define RG_GPIO_LCD_MISO            GPIO_NUM_19
 #define RG_GPIO_LCD_MOSI            GPIO_NUM_23
 #define RG_GPIO_LCD_CLK             GPIO_NUM_18
 #define RG_GPIO_LCD_CS              GPIO_NUM_5
-#define RG_GPIO_LCD_DC              GPIO_NUM_21
-#define RG_GPIO_LCD_BCKL            GPIO_NUM_14
-// #define RG_GPIO_LCD_RST           GPIO_NUM_NC
+#define RG_GPIO_LCD_DC              GPIO_NUM_2
+#define RG_GPIO_LCD_BCKL            GPIO_NUM_15
+// #define RG_GPIO_LCD_RST          GPIO_NUM_4
 
 // SPI SD Card
 #define RG_GPIO_SDSPI_MISO          GPIO_NUM_19
@@ -92,13 +77,9 @@
 #define RG_GPIO_SDSPI_CLK           GPIO_NUM_18
 #define RG_GPIO_SDSPI_CS            GPIO_NUM_22
 
-// External I2S DAC
-#define RG_GPIO_SND_I2S_BCK         GPIO_NUM_4
-#define RG_GPIO_SND_I2S_WS          GPIO_NUM_12
-#define RG_GPIO_SND_I2S_DATA        GPIO_NUM_15
-// #define RG_GPIO_SND_AMP_ENABLE      GPIO_NUM_NC
+// Audio - Internal DAC pins (built into ESP32, no wiring needed)
+// GPIO25 = Right channel, GPIO26 = Left channel
+// Connect a small speaker or headphone jack to GPIO25 + GND
 
 // Updater
-#define RG_UPDATER_ENABLE               1
-#define RG_UPDATER_APPLICATION          RG_APP_FACTORY
-#define RG_UPDATER_DOWNLOAD_LOCATION    RG_STORAGE_ROOT "/odroid/firmware"
+#define RG_UPDATER_ENABLE               0
